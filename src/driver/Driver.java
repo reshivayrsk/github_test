3b8ea352b2db098f7be870f1af24aa06fdf39cb5
poncoe <poncoe4mac@gmail.com>
2019-04-27 13:58:05 +0700

    hello world

--- src/driver/Driver.java
+++ src/driver/Driver.java
@@ -11,7 +11,7 @@ package driver;
  */
 public class Driver {
     public static void main(String[] args) {
-        System.out.println("");
+        System.out.println("Hello World");
     }
     
 }
